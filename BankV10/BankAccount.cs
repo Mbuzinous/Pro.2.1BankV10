7ebffbc7fcd8a17d960355ba6f384bc45b294f49
Kevin Waweru <kew001@edu.zealand.dk>
2024-03-04 13:01:33 +0100

    Add project files.

---
+++ BankV10/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.Remoting.Services;
+
+namespace BankV10
+{
+    public class BankAccount
+    {
+        #region Instance fields
+        private double _balance;
+        public string error = "enter a valid amount";
+        #endregion
+
+        #region Constructor
+        public BankAccount()
+        {
+            _balance = 0.0;
+        }
+        #endregion
+
+        #region Properties
+        public double Balance
+        {
+            get { return _balance; }
+        }
+        #endregion
+
+        #region Methods
+        //Need to make it so that method detects error in the logic and sends error msg instead
+        public void Deposit(double amount)
+        {
+            if (amount > 0.0)
+            {
+                _balance = _balance + amount;
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if ((_balance >= amount) && (amount >= 0.0) )
+            {
+                _balance = _balance - amount;
+            }
+        }
+        #endregion
+    }
+}
---
+++ BankV10/InsertCodeHere.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BankV10
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            BankAccount account = new BankAccount();
+            Console.WriteLine($"Balance is {account.Balance}");
+
+            account.Deposit(1000);
+            Console.WriteLine($"Balance is {account.Balance} after depositing 1000");
+            account.Withdraw(1001);
+            Console.WriteLine($"Balance is {account.Balance} after withdrawwing 1001 ---- Error, withdraw is larger than balance ---");
+            account.Withdraw(999);
+            Console.WriteLine($"Balance is {account.Balance} after withdrawwing 999");
+            
+
+            account.Deposit(-1000);
+            Console.WriteLine($"Balance is {account.Balance} after depositing -1000 ---- Error, cannot deposit minus nubmers ---");
+            account.Withdraw(-1000);
+            Console.WriteLine($"Balance is {account.Balance} after withdrawwing -1000 ---- Error, cannot withdraw minus nubmers ---");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
